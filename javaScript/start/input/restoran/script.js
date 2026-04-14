a8076d5de4245f4c1f2f466cb6b1e69d5e70f19e
Ahid <nagizadeahid@gmail.com>
2026-04-14 19:09:51 +0400

    Delete javaScript/start/input directory

--- javaScript/start/input/restoran/script.js
+++
@@ -1,8 +0,0 @@
-let btn =document.querySelector('.qiymet')
-let netice1 = document.querySelector('#netice')
-
-btn.onclick = function(){
-
-    let qiyme =yemek.value * 5
-    netice.innerHTML = qiyme
-}
